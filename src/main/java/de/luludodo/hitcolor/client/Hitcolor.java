691b670fb3f6cd40ee185035a70bfb537e18a8b6
agent LuluDodo <luludodo.gaming@gmail.com>
2023-06-22 20:38:33 +0200

    Initial

---
+++ src/main/java/de/luludodo/hitcolor/client/Hitcolor.java
@@ -0,0 +1,13 @@
+package de.luludodo.hitcolor.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Hitcolor implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
